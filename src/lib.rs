630804d6be4a8eb2cd4e4dd3a93ec45b5e2e0e09
Søren Mortensen <soren@neros.dev>
2021-02-20 16:52:11 +0000

    Update crate-level docs for 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,2 @@
 //! `n2` is a library implementation of a feedforward, backpropagation
 //! artificial neural network.
-//!
-//! **Note**: This is version `0.0.0` of this crate, which contains no
-//! functionality yet. Version `0.1.0` is currently being developed.
